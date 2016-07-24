e63b6ca6e42a925385ebf7409e2552db01240e07
Cara Donnelly <software@toadworld.co.uk>
2016-07-24 16:54:20 +0100

    Pull local copies of mailchimp and GA js for cache lifetime control.

---
+++ scripts/regenerate_js_cache.sh
@@ -0,0 +1,5 @@
+cd js/cached
+
+wget https://www.google-analytics.com/analytics.js && mv analytics.js.1 analytics.js
+wget https://s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js && mv mc-validate.js.1 mc-validate.js
+
