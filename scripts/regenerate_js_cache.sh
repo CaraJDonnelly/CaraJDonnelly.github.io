7801cb09acf2345d6466dd9f54f5e03df108e75e
Cara Donnelly <software@toadworld.co.uk>
2016-07-31 14:55:40 +0100

    Add scroll metrics via JQuery.

--- scripts/regenerate_js_cache.sh
+++ scripts/regenerate_js_cache.sh
@@ -2,4 +2,4 @@ cd js/cached
 
 wget https://www.google-analytics.com/analytics.js && mv analytics.js.1 analytics.js
 wget https://s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js && mv mc-validate.js.1 mc-validate.js
-
+wget https://code.jquery.com/jquery-3.1.0.min.js && mv jquery-3.1.0.min.js.1 jquery-3.1.0.min.js
